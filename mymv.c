0473d39e2a11778d828ecdd25af3f54c305268fc
Nourishere <nournawar5@gmail.com>
2025-03-18 22:01:50 +0200

    Updated mymv.c to return 1 for invalid arguments

--- mymv.c
+++ mymv.c
@@ -9,12 +9,17 @@ int main(int argc, char ** argv){
 	int fdsrc;
 	int fddest;
 	char buff[MAX_COUNT];
-	if(argc != 3)
+	if(argc != 3){
 		printf("%s: %s <source> <destination>\n", *argv,*argv);
+		return 1;
+	}
 	else{
 		fdsrc = open(argv[1],O_RDONLY);
-		if(fdsrc == -1)
+		if(fdsrc == -1){
 			printf("Error: Cannot open %s\n", argv[1]);
+			close(fdsrc);
+			return 1;
+		}
 		else{
 			fddest = open(argv[2],O_CREAT | O_WRONLY | O_TRUNC, 0644);
 			if(fddest== -1){
